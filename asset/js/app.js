744ce776d7b3a9a4ceac54a76f0982cbe6ae5c56
Infinidrix <biruksolomon11@gmail.com>
2021-02-15 14:46:19 +0300

    update comments

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,3 +1,3 @@
 import { init, testDB } from './db/db.js';
 
-document.addEventListener("DOMContentLoaded", testDB);
+document.addEventListener("DOMContentLoaded", testDB); // testDB only for testing purposes and init for other reasons
