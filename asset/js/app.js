f87416a45182a11809fab48aed77f05548058a90
abdi-edoc-de <se.abdulfeta.dedgba@gmail.com>
2021-02-17 23:12:04 +0300

    preparing the inputs for signup

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,20 +1,41 @@
-import { init, testDB, user_login } from './db/db.js';
+import { addUser, init, testDB, user_login } from './db/db.js';
 
-document.addEventListener("DOMContentLoaded", async () => {
-    
+document.addEventListener("DOMContentLoaded", async() => {
+
     await init();
+    // for login part
     const userNameInput = document.querySelector("#user")
     const passInput = document.querySelector("#pass")
     const showErr = document.querySelector("#showError")
     const spinnerLogin = document.querySelector(".spinner-border")
     const loginBtn = document.querySelector(".login")
-    let userName
-    
+
+    // for sgn up  as person part
+    const signupBtnPerson = document.querySelector("#signupBtnP")
+    const date = document.querySelector("#date")
+    const personConfirmPass = document.querySelector("#personConPass")
+    const personPass = document.querySelector("#personPass")
+    const personEmail = document.querySelector("#personGmail")
+    const lastName = document.querySelector("#lanme")
+    const firstName = document.querySelector("#fName")
+    const female = document.querySelector("#female")
+    const male = document.querySelector("#male")
+
+    // for company
+    const companyName = document.querySelector("#cName");
+    const emailCompany = document.querySelector("#emailCom")
+    const catgory = document.querySelector("#catgory")
+    const companyPass = document.querySelector("#cPass")
+    const signUpBntnComp = document.querySelector("#signUpBntnComp")
+    const confirmPassCompany = document.querySelector("#conCamPass")
+
+
     loginBtn.addEventListener("click", login)
-    
-    // user_login("se.biruk.solomon@gmail.com", "LifeIsShort").then(console.log)
+    signupBtnP.addEventListener("click", signUpPerson)
+    signUpBntnComp.addEventListener("click", signUpPerson)
+        // user_login("se.biruk.solomon@gmail.com", "LifeIsShort").then(console.log)
     async function login() {
-        userName = userNameInput.value
+        let userName = userNameInput.value
         let pass = passInput.value
         console.log("jiij");
         let user = await user_login(userName, pass)
@@ -22,29 +43,68 @@ document.addEventListener("DOMContentLoaded", async () => {
             spinnerLogin.style.display = "block"
         } else {
             passInput.style.border = "solid 1px"
-    
             userNameInput.style.border = "solid 1px"
             passInput.style.borderColor = "red"
             userNameInput.style.borderColor = "red"
             showErr.style.opacity = "1"
-    
             setTimeout(() => {
                 passInput.style.border = "none"
-    
                 userNameInput.style.border = "none"
-                passInput.style.borderColor = "red"
-                userNameInput.style.borderColor = "red"
                 showErr.style.opacity = "0"
-    
             }, 3000)
-    
-    
         }
     }
-    
-    function buraFun(u, p) {
-        return false
-    
-    }
-})
 
+    function signUpPerson(e) {
+        if (e.target.id === "signupBtnP") {
+            let firstName = firstName.value;
+            let lastName = lastName.value;
+            let bDate = date.value;
+            let conPass = personConfirmPass.value;
+            let pass = personPass.value;
+            let email = personEmail.value;
+            let gender = genderReveal();
+            user = addUser({
+                email: email,
+                password: pass,
+                type: "user",
+                tags: [bDate, gender, fName, ln]
+            })
+            if (user) {
+                localStorage.setItem("user", user);
+                location.href = "user.html";
+
+            } else {
+                alert("check again erorr")
+            }
+        } else {
+            let confirmPassCompany = confirmPassCompany.value
+            let companyPass = companyPass.value
+            let catgory = catgory.value
+            let emailCompany = emailCompany.value
+            let companyName = companyName.value
+            user = addUser({
+                email: emailCompany,
+                password: companyPass,
+                type: "company",
+                tags: [catgory]
+            })
+            if (user) {
+                localStorage.setItem("user", user);
+                location.href = "user.html";
+            } else {}
+        }
+    }
+
+    function genderReveal() {
+        if (female.value) {
+            gender = "feamle"
+        } else if (male.value) {
+            gender = "male"
+        }
+    }
+
+
+
+
+})
