5875871436974ad9c8310833b7a49d7f83dbd069
Biruk Solomon <41226525+Infinidrix@users.noreply.github.com>
2021-02-17 16:44:41 +0300

    Biruk.dummy data (#5)
    * improve data model
    
    * add dummy data
    
    * make [0] for no respondents for indexing purposes

--- asset/js/db/db.js
+++ asset/js/db/db.js
@@ -8,13 +8,16 @@ export async function init(){
 
   // creates the DB stores
   db.version(1).stores({
-    // autoincrementing ID, unique email, password, email and password indexed together for login, types and an array of tags
-    users: "++id,[&email+password],type,*tags", 
-    // user_id and survey_id as a compound primary key, and an array of answers
-    survey_results: "&[user_id+survey_id],*answers,date",
-    // autoincrementing ID, company_id, type, date, title, an array of tags, an array of questions, and the list of users that responded to it
-    surveys: "++id,company_id,type,date,title,*tags,*questions,*respondents"
+      // autoincrementing ID, unique email, password, email and password indexed together for login, types and an array of tags
+      users: "++id,[&email+password],type,*tags", 
+      // user_id and survey_id as a compound primary key, and an array of answers
+      survey_results: "&[user_id+survey_id],*answers,date",
+      // autoincrementing ID, company_id, type, date, title, an array of tags, an array of questions, and the list of users that responded to it
+      surveys: "++id,company_id,type,date,title,*tags,*questions,*respondents"
   });
+
+  // if the db is empty it will populate with dummy data
+  db.on("populate", populateDB);
 
   try {
     await db.open()
@@ -76,7 +79,7 @@ export async function user_login(email, password){
  * @returns {Object} The saved survey results object
  */
 export async function addSurveyResults(user_id, survey_id, answers){
-  let obj = {user_id, survey_id, answers}
+  let obj = {user_id, survey_id, answers, date: new Date()};
   try {
     // add the survey results
     let result = await db.survey_results.add(obj);
@@ -95,7 +98,7 @@ export async function addSurveyResults(user_id, survey_id, answers){
 
 
 /** Adds a survey object to the db,
- * @param {Object} survey - the survey to be saved. In format: {company_id:0, type:"", questions:[]}
+ * @param {Object} survey - the survey to be saved. In format: {company_id:0, type:"", questions:[], date: new Date(), respondents: []}
  * @returns {Object} The saved suvey
 */
 export async function addSurvey(survey){
@@ -154,3 +157,39 @@ export async function testDB(){
   await user_login("se.biruk.solomon@gmail.com", "amazingPassword");
 }
 
+function populateDB(){
+  addUser({email: "se.biruk.solomon@gmail.com", password:"LifeIsShort", type:"user", tags: ["education", "economics"]});
+  addUser({email: "se.hanan.miftah@gmail.com", password:"SuperSecurePassword", type:"user", tags: ["education", "health"]});
+  addUser({email: "se.natneam.mesele@gmail.com", password:"DuplicatePassword", type:"user", tags: ["education", "phliosophy"]});
+  addUser({email: "se.abdulfeta.dedgba@gmail.com", password:"DuplicatePassword", type:"user", tags: ["education", "gaming", "business"]});
+  addUser({email: "marketer@company.org", password:"BusinessCasual", type:"company", tags: ["gaming", "economics"]});
+  addUser({email: "ngo@charity.com", password:"HelpEveryone", type:"company", tags: ["education", "health"]});
+  addSurvey({company_id: 5, type:"poll", date: new Date(2020, 12, 3), title:"Do you think that games are good for the economy", respondents: [0, 1, 4]});
+  addSurvey({company_id: 6, type:"poll", date: new Date(2020, 12, 4), title:"Would you consider participating in a fundraiser for building better school facilites for underprevilaged kids", respondents: [0]});
+  addSurvey({company_id: 5, type:"poll", date: new Date(2020, 12, 5), title:"Would you be interested in working in a game development company if training is provided", respondents: [0]});
+  addSurvey({company_id: 6, type:"poll", date: new Date(2020, 12, 6), title:"Increase government budget for public hospitals!!", respondents: [0]});
+  addSurvey({company_id: 5, type:"survey", date: new Date(2020, 12, 6, 4), 
+    title:"Would you play games developed by Ethiopians", 
+    description: "How do you feel about playing games by Ethiopians for Ethiopians. We're talking about fighting through Adwa and playing yegena chewata",
+    questions:[
+      {type:"option", questions: "Have you tried Ethiopian games before", options:["Yes", "No"]},
+      {type:"text", questions: "What do you feel about the quality of games that Ethiopian publishers can provide", options:[]},
+    ],
+    respondents: [4]
+  });
+  addSurvey({company_id: 6, type:"survey", date: new Date(2020, 12, 6, 4), 
+    title:"What's the health coverage like in Ethiopia", 
+    description: "We want to know about any difficulties or good results you've had from public hospitals in Ethiopia",
+    questions:[
+      {type:"option", questions: "Have you ever been to a public hospital in Ethiopia", options:["Yes", "No"]},
+      {type:"text", questions: "How was the quality of the doctors", options:[]},
+      {type:"text", questions: "How was the quality of other staff", options:[]},
+      {type:"text", questions: "How was the quality of the equipment", options:[]},
+      {type:"text", questions: "How was the overall experience", options:[]},
+    ],
+    respondents: [0]
+  });  
+  addSurveyResults(4, 1, ["Agree"]);
+  addSurveyResults(1, 1, ["Disagree"]);
+  addSurveyResults(4, 5, [0, "I don't feel like it's in a good place now but there's a lot of room to improve and I'd be glad to play and test them out."]);
+}
