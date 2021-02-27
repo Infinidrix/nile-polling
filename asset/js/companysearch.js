5da2bbdc25a97c7dea8ed87bed909e49dc7cf47c
natneam <se.natneam.mesele@gmail.com>
2021-02-27 12:07:12 +0300

    adding search functionality to the compnay page

--- asset/js/companysearch.js
+++ asset/js/companysearch.js
@@ -1,19 +1,21 @@
-import {searchSurveys} from './db/db.js'
-import {displaySurveys} from './userpage.js'
-
+import {searchSurveys, user_login} from './db/db.js'
+import {displaySurveys} from './companypage.js'
 
 async function search(inp) {
     inp.addEventListener("input", async function(e) {
+        console.log("fhsjk")
         let val = this.value;
         let arr = await searchSurveys(val);
         // empty the display list
         document.getElementById('survey-content').innerHTML = ""
         /*for each item in the array...*/
         for (let i = 0; i < arr.length; i++) {
-          console.log(arr[i])
-          displaySurveys(arr[i]);
+            if (arr[i].company_id === user.id){
+                displaySurveys(arr[i]);
+            }
+            // displaySurveys(arr[i]);
         }
     });
-  }
-  search(document.getElementById("search-focus"));
-// console.log(document.querySelector("#search-focus"));
+}
+
+search(document.getElementById("search-focus"));
