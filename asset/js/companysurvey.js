cc724f4767bee5b76a0a84757eefedddf0b3fddb
natneam <se.natneam.mesele@gmail.com>
2021-03-03 22:17:27 +0300

    multiple choice customization added

--- asset/js/companysurvey.js
+++ asset/js/companysurvey.js
@@ -9,7 +9,7 @@ let questions = []
 let addSurveyButton = `
 <div class="row pb-4">
     <div class="col-md-12 text-end" style="flex: right;">
-        <i class=" fa fa-plus p-2 " id="add_survey_entry" aria-hidden="true " style="color: white; background-color:#2867B2;border-radius:50% "></i>
+        <i class=" fa fa-plus p-2 " id="add_survey_entry" aria-hidden="true " style="color: white; background-color:rgba(9, 190, 173, 1);border-radius:50% "></i>
     </div>
 </div>`
 
@@ -18,16 +18,11 @@ let multipleChoice = `
     <div class="col-md-6 py-2">
         <input class="form-control  id="option_1" form-control-lg my-1" type="text" placeholder="Option 1" aria-label=".form-control-lg example">
     </div>
-    <div class="col-md-6 py-2">
-        <input class="form-control  id="option_2" form-control-lg my-1" type="text" placeholder="Option 2" aria-label=".form-control-lg example">
-    </div> 
-    <div class="col-md-6 py-2">
-        <input class="form-control  id="option_3" form-control-lg my-1" type="text" placeholder="Option 3" aria-label=".form-control-lg example">
-    </div>
-    <div class="col-md-6 py-2">
-        <input class="form-control  id="option_4" form-control-lg my-1" type="text" placeholder="Option 4" aria-label=".form-control-lg example">
-    </div> 
-</div>`
+</div>
+<div class="col-md-12 text-end" style="flex: right;">
+    <i class=" fa fa-plus p-2 " id="add_multiple_choice_entry" aria-hidden="true " style="color: white; background-color:rgba(9, 190, 173, 1);border-radius:50% "></i>
+</div>
+`
 
 let textReplay = `
 <div class="row py-3">
@@ -62,7 +57,7 @@ function addSurveyEntryFunc(e) { // function to add new question entry
             <div class="form-check">
                 <div class="row py-3">
                     <div class="dropdown ">
-                                <button style="background-color: #2867B2;border:none" class="btn btn-lg  btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                <button style="background-color: rgba(9, 190, 173, 1);border:none" class="btn btn-lg  btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
                                 Responding Method
                                 </button>
                                 <ul class=" dropdown-menu dropdown-menu-dark " style="background-color: #ffffff; color:black" aria-labelledby=" dropdownMenuButton2 ">
@@ -91,37 +86,44 @@ function selectAnsweringMethod(e) { // function to select question responding ty
     // console.log(e.target.innerHTML)
     if (e.target.innerHTML === `Multiple Options`){
         e.target.parentNode.parentNode.parentNode.parentNode.children[1].innerHTML = multipleChoice;
+        document.getElementById('add_multiple_choice_entry').addEventListener('click', addMultipleChoiceEntryFunc);
+
     }
     else if (e.target.innerHTML === `Text Reply`){
         e.target.parentNode.parentNode.parentNode.parentNode.children[1].innerHTML = textReplay;
     }
 }
 
+function addMultipleChoiceEntryFunc(e){
+    let optionCount = e.target.parentNode.parentNode.children[0].children.length + 1
+    e.target.parentNode.parentNode.children[0].insertAdjacentHTML ('beforeend', `<div class="col-md-6 py-2">
+    <input class="form-control  id="option_${optionCount}" form-control-lg my-1" type="text" placeholder="Option ${optionCount}" aria-label=".form-control-lg example">
+</div>`)
+}
+
+
 function saveSurvey(e) { // function to save survey format to the db
     let questions = []
     for (let i=1; i < questionCount+1; i++){
         let question = document.getElementById("question_" + i);
-        if (question.children[0].children[1].children[0].children[0].children[0].innerHTML !== ""){
-            alert("Error: You have to chose responding method")
-        }else{
-            let type = 1
-            if (question.children[0].children[1].children[0].children.length === 4){
-                type = 4
-            }
-            // console.log(question.children[0].children[1].children[0].children[i-1].children[0].value)
-            let answeringMethodData = []
-            for (let j = 0; j < type; j++){
-                answeringMethodData.push(question.children[0].children[1].children[0].children[j].children[0].value)
-            }
 
-            let questionObj = {
-                "questions" : question.children[0].children[0].value,
-                "type": type==1?'text':'option',
-                "options" : answeringMethodData
-            }
-            questions.push(questionObj)
+        let type = 1
+        if (question.children[0].children[1].children[0].children[1].children[0].children.length > 1){
+            type = question.children[0].children[1].children[0].children[1].children[0].children.length
         }
-    }
+        // console.log(question.children[0].children[1].children[0].children[i-1].children[0].value)
+        let answeringMethodData = []
+        for (let j = 0; j < type; j++){
+            answeringMethodData.push(question.children[0].children[1].children[0].children[1].children[0].children[j].children[0].value)
+        }
+
+        let questionObj = {
+            "questions" : question.children[0].children[0].value,
+            "type": type==1?'text':'option',
+            "options" : answeringMethodData
+        }
+        questions.push(questionObj)
+}
     let survey = {
         "company_id" : window.user.id,
         "title" : document.getElementById("title_answer").value,
@@ -136,7 +138,7 @@ function saveSurvey(e) { // function to save survey format to the db
         addSurvey(survey)
             .then((result)=>{
                 console.log(result)
-                history.back()
+                // history.back()
             }, (result)=>{
                 console.log("error" + result)
             });
