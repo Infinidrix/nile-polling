0dc1bc9764cd5411005fff7ab0fcefcf22b9b523
abdi-edoc-de <se.abdulfeta.dedgba@gmail.com>
2021-03-04 08:58:10 +0300

    js part for edit pages and change password pages

---
+++ asset/js/editForComapnyProfile.js
@@ -0,0 +1,33 @@
+import { addUser, init, testDB, user_login } from './db/db.js';
+
+document.addEventListener("DBInitalized", async() => {
+
+    // const edit = document.querySelector("#edit")
+    const gmail = document.querySelector("#gmail")
+    const catagory = document.querySelector("#catagory")
+    const comName = document.querySelector("#comName")
+    const svaeComapny = document.querySelector("#svaeComapny")
+    comName.value = "Company 1"
+    gmail.value = user.email
+    catagory.value = user.tags[0]
+    console.log(user.email)
+    var pass = user.password
+
+    // user_login
+    svaeComapny.addEventListener("click", saveCompany)
+
+    async function saveCompany(e) {
+        //   burra function
+        let user = await addUser({
+            email: gmail.value,
+            password: pass,
+            type: "company",
+            tags: [catagory.value]
+        })
+        console.log("here")
+    }
+
+
+
+
+})
---
+++ asset/js/editForUser.js
@@ -0,0 +1,60 @@
+import { addUser, init, testDB, user_login } from './db/db.js';
+
+document.addEventListener("DBInitalized", async() => {
+
+    // const edit = document.querySelector("#edit")
+    const gmail = document.querySelector("#gmail")
+    const fName = document.querySelector("#fName")
+    const lName = document.querySelector("#lName")
+    const female = document.querySelector("#female")
+    const male = document.querySelector("#male")
+    const date = document.querySelector("#date")
+    const svae = document.querySelector("#svae")
+        // comName.value = "Company 1"
+        // gmail.value = user.email
+        // catagory.value = user.tags[0]
+        // console.log(user.email)
+
+    // user_login
+    console.log(user.email)
+    gmail.value = user.email
+    date.value = user.tags[0]
+    fName.value = "Abdi"
+    lName.value = "De"
+    save.addEventListener("click", saveUser)
+    chckingGender()
+
+    function saveUser(e) {
+        //   burra function
+        let user = await addUser({
+            email: email,
+            password: pass,
+            type: "user",
+            tags: [date.value, genderReveal(), fName.value, lName.value]
+        })
+    }
+
+    function genderReveal() {
+
+        if (female.checked) {
+            return "female"
+        } else if (male.checked) {
+            return "male"
+        }
+    }
+
+    function chckingGender(gender) {
+        if (user.tags[1] === "male") {
+            male.checked = true
+
+        }
+        if (user.tags[1] === "female") {
+            male.checked = true
+
+        }
+
+    }
+
+
+
+})
